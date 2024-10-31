f340e9c48446e08f3cb0d472996db3f4c0014028
Zeth Porter <zethwporter@gmail.com>
2024-10-30 20:05:07 -0600

    teachers quorum updates (#1)

--- src/assets/PokemonCard.tsx
+++ src/assets/PokemonCard.tsx
@@ -2,16 +2,18 @@ import { type Pokemon } from "pokenode-ts";
 
 const PokemonCard = ({ pokemon }: { pokemon: Pokemon }) => {
   return (
-    <div className="card bg-base-200 shadow-xl w-56 p-4">
-      <h2 className="card-title capitalize pb-2">{pokemon.name}</h2>
+    <div className="card bg-yellow-500 shadow-xl w-56 border-4 p-4 border-yellow-700">
+      <h2 className="card-title capitalize pb-2 text-blue-900">
+        {pokemon.name}
+      </h2>
       <img
-        className="bg-base-300 rounded-btn"
+        className="bg-yellow-100 rounded-btn"
         src={pokemon.sprites.front_default ?? ""}
         alt={pokemon.name}
       />
-      {/* <div className="card-body">
-        <p>{pokemon.weight}</p>
-      </div> */}
+      <div className="text-blue-900 font-bold pt-2">
+        <p>{`Weight: ${pokemon.weight}`}</p>
+      </div>
     </div>
   );
 };
